ba5466c9a8b329bcb8d5c4c17a88843fec3bac88
longxiao <364863974@qq.com>
2019-11-11 11:36:03 +0800

    first commit

---
+++ src/main/java/com/example/lxtest01/LxTest01Application.java
@@ -0,0 +1,13 @@
+package com.example.lxtest01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LxTest01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LxTest01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lxtest01/LxTest01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lxtest01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LxTest01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
